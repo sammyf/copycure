82e0f52be5b36af2023f488efef126b5cecec68c
sammy fischer <samuel.fischer@smartinsurtech.de>
2025-01-06 10:58:41 +0100

    flag to switch between memory or sqlite3 useage

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"crypto/sha256"
 	"database/sql"
+	"flag"
 	"fmt"
 	_ "github.com/mattn/go-sqlite3"
 	"io"
@@ -17,6 +18,9 @@ func printOverwrite(message string) {
 func initDB() (*sql.DB, error) {
 	dbPath := "/tmp/copycure.db"
 	err := os.Remove(dbPath)
+	if err != nil && !os.IsNotExist(err) {
+		return nil, err
+	}
 	db, err := sql.Open("sqlite3", dbPath)
 	if err != nil {
 		return nil, err
@@ -67,8 +71,7 @@ func countFiles(directory string) (int, error) {
 	return totalFiles, err
 }
 
-func removeDuplicates(db *sql.DB, directory string, allFiles int) (int, error) {
-	// seenChecksums := make(map[string]struct{})
+func removeDuplicates(db *sql.DB, seenChecksums map[string]struct{}, directory string, allFiles int) (int, error) {
 	cnt := 0
 	pct := 0
 	fileCnt := 0
@@ -94,39 +97,42 @@ func removeDuplicates(db *sql.DB, directory string, allFiles int) (int, error) {
 				return nil
 			}
 
-			var existingPath string
-			err = db.QueryRow("SELECT path FROM files WHERE checksum = ?", checksum).Scan(&existingPath)
-			if err == sql.ErrNoRows {
-				// No duplicate found
-				_, err = db.Exec("INSERT INTO files (checksum, path) VALUES (?, ?)", checksum, path)
-				if err != nil {
-					fmt.Println("failed inserting checksum  for file %s: %v\n", path, err)
-				}
-			} else if err != nil {
-				fmt.Println()
-				fmt.Printf("Failed to query database for file %s: %v\n", path, err)
-				return nil
-			} else {
-				err := os.Remove(path)
-				if err != nil {
+			if db != nil {
+				var existingPath string
+				err = db.QueryRow("SELECT path FROM files WHERE checksum = ?", checksum).Scan(&existingPath)
+				if err == sql.ErrNoRows {
+					// No duplicate found
+					_, err = db.Exec("INSERT INTO files (checksum, path) VALUES (?, ?)", checksum, path)
+					if err != nil {
+						fmt.Println("failed inserting checksum for file %s: %v\n", path, err)
+					}
+				} else if err != nil {
 					fmt.Println()
-					fmt.Printf("Failed to remove duplicate file %s: %v\n", path, err)
+					fmt.Printf("Failed to query database for file %s: %v\n", path, err)
 					return nil
+				} else {
+					err := os.Remove(path)
+					if err != nil {
+						fmt.Println()
+						fmt.Printf("Failed to remove duplicate file %s: %v\n", path, err)
+						return nil
+					}
+					cnt++
 				}
-				cnt++
+			} else if seenChecksums != nil {
+				if _, exists := seenChecksums[checksum]; exists {
+					err := os.Remove(path)
+					if err != nil {
+						fmt.Println()
+						fmt.Printf("Failed to remove duplicate file %s: %v\n", path, err)
+						return nil
+					}
+					cnt++
+				} else {
+					seenChecksums[checksum] = struct{}{}
+				}
 			}
 
-			//if _, exists := seenChecksums[checksum]; exists {
-			//	err := os.Remove(path)
-			//	if err != nil {
-			//		fmt.Println()
-			//		fmt.Printf("Failed to remove duplicate file %s: %v\n", path, err)
-			//		return nil
-			//	}
-			//	cnt++
-			//} else {
-			//	seenChecksums[checksum] = struct{}{}
-			//}
 		}
 		return nil
 	})
@@ -140,19 +146,36 @@ func removeDuplicates(db *sql.DB, directory string, allFiles int) (int, error) {
 }
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: go run script.go /path/to/your/directory")
+	var directory string
+	var mode string
+
+	flag.StringVar(&directory, "i", "", "Directory to scan for duplicates")
+	flag.StringVar(&mode, "m", "sql", "Mode: 'sql' or 'mem'")
+	flag.Parse()
+
+	if directory == "" {
+		fmt.Println("Usage: go run script.go -i /path/to/your/directory [-m sql|mem]")
 		os.Exit(1)
 	}
 
-	directory := os.Args[1]
+	var db *sql.DB
+	var seenChecksums map[string]struct{}
 
-	db, err := initDB()
-	if err != nil {
-		fmt.Printf("Error initializing database: %v\n", err)
+	switch mode {
+	case "sql":
+		var err error
+		db, err = initDB()
+		if err != nil {
+			fmt.Printf("Error initializing database: %v\n", err)
+			os.Exit(1)
+		}
+		defer db.Close()
+	case "mem":
+		seenChecksums = make(map[string]struct{})
+	default:
+		fmt.Println("Invalid mode. Use 'sql' or 'mem'.")
 		os.Exit(1)
 	}
-	defer db.Close()
 
 	totalFiles, err := countFiles(directory)
 	if err != nil {
@@ -160,7 +183,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	cnt, err := removeDuplicates(db, directory, totalFiles)
+	cnt, err := removeDuplicates(db, seenChecksums, directory, totalFiles)
 	if err != nil {
 		fmt.Printf("Error removing duplicates: %v\n", err)
 		os.Exit(1)
